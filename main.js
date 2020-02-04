218b526bd80d7514caa35608d36dac4a093c04a4
Łukasz Szmyd <szmydlukasz30@gmail.com>
2020-02-04 21:52:47 +0100

    javascript functionality

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ const form = document.querySelector('form');
 const username = document.querySelector('.username');
 const password = document.querySelector('.password');
 const button = document.querySelector('button');
+const messageOutput = document.querySelector('.message');
 
 
-
+console.log(messageOutput);
 
 
 form.addEventListener('submit', e => {
@@ -13,7 +14,12 @@ form.addEventListener('submit', e => {
 })
 
 function validation() {
+
  if (username.value === '' || username.value == null) {
-     console.log('duba');
+     messageOutput.innerHTML = "Username can't be blank"
+ }
+
+ if (password.value.length <=5) {
+    messageOutput.innerHTML = "Password must be 5 or more characters"
  }
 }
